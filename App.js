020d025620675fcd8e57848709c1af22fd379c44
Melissa Bernstein <43127281+melissab1238@users.noreply.github.com>
2023-12-01 13:25:13 -0600

    First commit: basic React Native x Expo app

--- App.js
+++ App.js
@@ -5,6 +5,11 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello world!</Text>
+      <Text style={styles.red}>just red</Text>
+      <Text style={styles.bigBlue}>just bigBlue</Text>
+      <Text style={[styles.bigBlue, styles.red]}>bigBlue, then red</Text>
+      <Text style={[styles.red, styles.bigBlue]}>red, then bigBlue</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +21,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 50,
+  },
+  bigBlue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
   },
 });
